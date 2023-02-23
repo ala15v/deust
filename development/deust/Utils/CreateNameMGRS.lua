9aa7b8e1dc00b8289d62ecb4f22ade7010d2b26f
Alejandro Sanz Martin <alexsm32@gmail.com>
2023-02-23 11:53:39 +0200

    feat(CreateNameMGRS): Input validation

--- development/deust/Utils/CreateNameMGRS.lua
+++ development/deust/Utils/CreateNameMGRS.lua
@@ -1,12 +1,24 @@
-
-
 function CreateNameMGRS(prefix, coord)
-    local mgrs = {}
-    for value in string.gmatch(coord:GetName(), "%S+") do
-        table.insert(mgrs, value)
-    end
-    return mgrs[3] .. string.sub(mgrs[4], 1, 1) .. string.sub(mgrs[5], 1, 1) .. "_" .. prefix
-end
+    -- ANCHOR: Input validation
+    local invalidInput = false
 
+    if type(prefix) ~= "string" then
+        invalidInput = true
+    end
+    if type(coord) ~= "table" or coord.ClassName ~= "COORDINATE" then
+        invalidInput = true
+    end
+
+    -- ANCHOR: Start point
+    if not invalidInput then
+        local mgrs = {}
+        for value in string.gmatch(coord:GetName(), "%S+") do
+            table.insert(mgrs, value)
+        end
+        return mgrs[3] .. string.sub(mgrs[4], 1, 1) .. string.sub(mgrs[5], 1, 1) .. "_" .. prefix
+    else
+        env.error("Deust CreateNameMGRS: Invalid Input")
+    end
+end
 
 deust.utils.CreateNameMGRS = true
