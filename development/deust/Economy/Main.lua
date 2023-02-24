61e39fa69c51469c1178ecf8492d5d3f212ef67d
Alejandro Sanz Martin <alexsm32@gmail.com>
2023-02-24 12:44:50 +0200

    fix(Economy): FSM

--- development/deust/Economy/Main.lua
+++ development/deust/Economy/Main.lua
@@ -6,6 +6,7 @@ Economy = {
     Funds = 0,
     Industry = {},
     DefenseBudget = 0, -- percent
+    SavePath = lfs.writedir() .. "Economy\\",
     SaveFile = nil,
     Running = false
 }
@@ -14,6 +15,9 @@ Economy = {
 function Economy:New(alias, coalition)
     -- Input validation
     local invalidInput = false
+
+    -- Inherit everthing from FSM class.
+    local self = BASE:Inherit(self, FSM:New()) -- #Economy
 
     if type(alias) ~= "string" then
         invalidInput = true
@@ -32,8 +36,18 @@ function Economy:New(alias, coalition)
         self:SetStartState("NotReadyYet")
 
         -- Add FSM transitions.
-        --                 From State   -->   Event        -->     To State
-        self:AddTransition("NotReadyYet", "Load", "Loaded") -- Load the warehouse state from scatch.
+        --                 From State   -->         Event        -->     To State
+        self:AddTransition("NotReadyYet", "Load", "Loaded") -- Load the Economy state from scatch.
+        self:AddTransition("Stopped", "Load", "Loaded") -- Load the Economy state stopped state.
+
+        self:AddTransition("NotReadyYet", "Start", "Running") -- Start the Economy from scratch.
+        self:AddTransition("Loaded", "Start", "Running") -- Start the Economy when loaded from disk.
+
+        self:AddTransition("*", "Stop", "Stopped") -- Stop the Economy.
+        self:AddTransition("Running", "Pause", "Paused") -- Pause the processing of new requests.
+        self:AddTransition("Paused", "Unpause", "Running") -- Unpause the Economy. Queued requests are processed again.
+
+        self:AddTransition({ "Paused", "Stopped" }, "Save", "*") -- Save the Economy state to disk.
         -- !SECTION
     else
         env.error("Deust Economy.New(): Invalid Input")
