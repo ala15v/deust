0551dc77f7a71b7d7a82f9351b955d26ef992e59
Alejandro Sanz Martin <alexsm32@gmail.com>
2023-03-06 21:24:12 +0200

    feat(Economy): Added Economy documentation

--- development/deust/Economy/Main.lua
+++ development/deust/Economy/Main.lua
@@ -1,3 +1,215 @@
+--[[
+
+# Economy
+
+This class performes as a bank. It offers methods and events to handle the movement of fictional money. To get the best experience, this class should be use in convination with other classes and functions. The class also offers a way to save in disk and restore the current state of the economy.
+
+## Economy:New(alias, coalition)
+
+It initiate a new instance of the class Economy
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |         Options         |                                     Description                                         |
+> |:-----------------------:|:-----------------:|:-----------------------:|:----------------------------------------------------------------------------------------|
+> |         alias           |       String      |                         | This is the name that the object will be known by                                       |
+> |       coalition         |       String      | *`"blue"`* \| *`"red"`* | This is the coalition name                                                              |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |     Economy      |            Table              | New instance of the class Economy                            |
+
+> ### **Example**
+>
+>> Input: `MyEconomy = Economy:New("OP_Bank", "blue")`
+
+## Economy:SetFunds(value)
+
+It set the funds available. It should be only used during the creation to set the initial ammount of funds.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                                     Description                                         |
+> |:-----------------------:|:-----------------:|:----------------------------------------------------------------------------------------|
+> |         value           |      Integer      | This is the ammount of money that will be available                                     |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:SetFunds(3200)`
+
+## Economy:IncreaseFunds(value)
+
+It increases the funds available. It will take the current funds and add the given ammount.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                                     Description                                         |
+> |:-----------------------:|:-----------------:|:----------------------------------------------------------------------------------------|
+> |         value           |      Integer      | This is the ammount of money that will be added to the current ammount of funds         |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:IncreaseFunds(200)`
+
+## Economy:DecreaseFunds(value)
+
+It decreases the funds available. It will take the current funds and subtract the given ammount.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                                     Description                                         |
+> |:-----------------------:|:-----------------:|:----------------------------------------------------------------------------------------|
+> |         value           |      Integer      | This is the ammount of money that will be subtracted to the current ammount of funds    |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:DecreaseFunds(200)`
+
+## Economy:SetTransactionsDelay(seconds)
+
+It set the ammount of seconds between the processing of each transaction.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |    Default    |                                     Description                                             |
+> |:-----------------------:|:-----------------:|:-------------:|:--------------------------------------------------------------------------------------------|
+> |        seconds          |      Integer      |     `30`      | This is the ammount of seconds that the class will wait before process the next transaction |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:SetTransactionsDelay(60)`
+
+## Economy:SetSavePath(value)
+
+It set the path where the file with the current state will be saved. The path must exist.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                           Default                           |                                     Description                                             |
+> |:-----------------------:|:-----------------:|:-----------------------------------------------------------:|:--------------------------------------------------------------------------------------------|
+> |        value            |      String       |     `C:\Users\<user>\Saved Games\DCS.openbeta\Economy`      | This is the path where the file with the current state will be saved                        |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:SetSavePath("C:\\folder\\")`
+
+## Economy:SetSaveFile(value)
+
+It set the file name where the current state will be saved.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |           Default              |                                     Description                                             |
+> |:-----------------------:|:-----------------:|:------------------------------:|:--------------------------------------------------------------------------------------------|
+> |        value            |      String       |     `<Alias>_Account.lua`      | This is the file name where the current state will be saved                                 |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:SetSaveFile("OP_Bank.lua")`
+
+## Economy:SetAutoSave(value)
+
+It set the ammount of seconds between each auto save. If nil, the autosave is disabled.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                                     Description                                             |
+> |:-----------------------:|:-----------------:|:--------------------------------------------------------------------------------------------|
+> |        value            |      Integer      | This is the ammount of seconds between each auto save                                       |
+
+> ### **Return**
+>
+> |     Name        |             Type               |                       Description                            |
+> |:---------------:|:------------------------------:|:-------------------------------------------------------------|
+> |                 |            Boolean             | It returns true if the process went well                     |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:SetAutoSave(300)`
+
+## Economy:Load()
+
+This load the previous state from the save file. This method must be called before the `:Start()` method.
+
+> ### **Example**
+>
+>> Input: `MyEconomy:Load()`
+
+## Economy:Start()
+
+This starts the normal work of the object.
+
+> ### **Example**
+>
+>> Input: `MyEconomy:Start()`
+
+> ### **Example**
+>
+> Using a delay of 5 seconds
+>
+>> Input: `MyEconomy:__Start(5)`
+
+## Economy:AddTransaction(Transaction)
+
+It adds to the transaction queue a new transaction.
+
+> ### **Parameters**
+>
+> |         Name            |       Type        |                                     Description                                             |
+> |:-----------------------:|:-----------------:|:--------------------------------------------------------------------------------------------|
+> |     Transaction         |       Table       | This is the Transaction object that will be added to the queue                              |
+
+> ### **Example**
+>
+>> Input: `MyEconomy:AddTransaction(Transaction)`
+
+> ### **Example**
+>
+> Using a delay of 5 seconds
+>
+>> Input: `MyEconomy:__AddTransaction(5, Transaction)`
+
+]]--
+
+
 -- ANCHOR: Class declaration
 Economy = {
     ClassName = "Economy",
