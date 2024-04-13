7f7366180282cbf12cc7fe1260d0de3972fe93f5
mac-developer <macd55@gmail.com>
2024-04-13 15:32:27 +0200

    feat(menu): deust replaced by ALA15V Blue

--- development/deust/Menu/Menu.lua
+++ development/deust/Menu/Menu.lua
@@ -1,7 +1,8 @@
 -------------------
 -- CUSTOM MENUS  --
 -------------------
-MenudeustManagement = MENU_COALITION:New( coalition.side.BLUE, 'deust Menu' )
+MenuALA15VBlue = MENU_COALITION:New( coalition.side.BLUE, 'ALA15V Blue' )
+MenudeustManagement = MENU_COALITION:New( coalition.side.BLUE, 'deust Menu', MenuALA15VBlue )
 FatCowCurrentPosition = MENU_COALITION_COMMAND:New(coalition.side.BLUE, 'FatCow Position', MenudeustManagement, deust.fatcow.getPosition, {})
 FotCowEstimatedTime = MENU_COALITION_COMMAND:New(coalition.side.BLUE, 'FatCow ETA', MenudeustManagement, deust.fatcow.ETAtoLanding, {})
 FotCowLZCoords = MENU_COALITION_COMMAND:New(coalition.side.BLUE, 'FatCow LZ Coords', MenudeustManagement, deust.fatcow.GetLLDDM, {})
